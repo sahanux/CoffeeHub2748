8ca94eb70e12ea3a228ee0d7aa606c295d724fdd
Sahan Akalanka <82861842+sahanux@users.noreply.github.com>
2023-11-23 20:31:57 +0530

    files updated

--- Project/coffee-ordering-app/theme/index.js
+++
@@ -1,9 +0,0 @@
-export const themeColors = {
-   // bgColor: opacity=> `rgba(145, 73, 0, ${opacity})`,
-   // bgColor: opacity=> `rgba(65, 2, 0, ${opacity})`,
-   bgColor: opacity=> `rgba(141, 95, 71, ${opacity})`,
-   bgLight: '#2A6044',
-   bgDark: '#014B25',
-   text: '#3C2A21'
-   
-}   
